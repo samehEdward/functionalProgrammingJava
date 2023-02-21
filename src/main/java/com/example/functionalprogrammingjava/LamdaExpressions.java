14428f8b41d22cf943cd7ee8531b760200a7cfe0
Sameh Edward <edw21629@spengergasse.at>
2023-02-21 13:27:03 +0100

    Lambda Kurz

--- src/main/java/com/example/functionalprogrammingjava/LamdaExpressions.java
+++ src/main/java/com/example/functionalprogrammingjava/LamdaExpressions.java
@@ -1,6 +1,15 @@
 package com.example.functionalprogrammingjava;
 
+import java.util.function.Function;
+
 public class LamdaExpressions {
 
+    public static void main(String[] args) {
+
+        Function<Integer , Integer> absloutValue =
+                (x) -> x < 0 ? x : -x;
+
+        System.out.println(absloutValue.apply(-10));
+    }
 
 }
